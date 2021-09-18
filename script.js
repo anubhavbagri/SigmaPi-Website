def2c89b4f86b4fb147c64b797e31a203b30f467
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-09-18 08:18:30 +0530

    feat: content abstraction using js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("intro-to-c").innerText =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, dolorem voluptas! Repellat maxime, inventore, est laborum recusandae vel molestiae ab possimus unde asperiores eos voluptatum voluptatibus velit officia vero. Perspiciatis ex quidem maxime itaque dolores possimus rerum sit omnis porro earum tempora dolorem obcaecati deserunt est impedit dignissimos, accusamus, ipsam ullam recusandae, quod quaerat cum illo sequi inventore.";
+
+document.getElementById("datatypes").innerText =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, dolorem voluptas! Repellat maxime, inventore, est laborum recusandae vel molestiae ab possimus unde asperiores eos voluptatum voluptatibus velit officia vero.";
+
+document.getElementById("loops").innerText =
+  "ipsum dolor sit amet, consectetur adipisicing elit. Officiis, nam temporibus provident voluptas ducimus, saepe reprehenderit sapiente eius, molestias assumenda voluptates cum similique commodi itaque necessitatibus in nulla corporis recusandae nesciunt culpa natus? Architecto officiis at sapiente consequuntur id perspiciatis, quam possimus et ipsam perferendis ab recusandae unde illo sunt maxime cumque consectetur alias. Aut sapiente illum perspiciatis sed, ut dolorem rem reprehenderit cumque iste labore illo laboriosam debitis natus quis fugit rerum atque impedit voluptate! Illum quidem distinctio ipsum culpa debitis nesciunt reiciendis rem consequatur, soluta nostrum maiores velit magnam, natus minus ipsam quia, dolores accusantium aspernatur voluptate adipisci.";
+
+document.getElementById("arrays").innerText =
+  "adipisci obcaecati, recusandae similique repellendus quidem iste at eos, dolores, ex quasi error excepturi molestiae? Vel aliquid harum necessitatibus tempore voluptas doloribus reprehenderit sed ipsum, dignissimos suscipit! Pariatur vero sed veniam id earum iste, harum ducimus odit nesciunt, fuga laborum deserunt quasi ipsum. Saepe laborum qui, corrupti possimus, porro aspernatur est architecto dignissimos cumque ipsum consequatur doloremque?";
